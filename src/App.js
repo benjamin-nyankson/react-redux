fe7af5482830c03f593bb196bab2ff9b4070843d
nyankson-benjamin <benjamin.nyankson@amalitech.com>
2023-02-13 08:34:55 -0800

    NEW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      {amount > 0 ? (
+        <h1 style={{ color: "lime" }}>You have {amount} GHC in your account</h1>
+      ) : amount === 0 ? (
+        <h1 style={{ color: "white" }}>
+          You have {amount} GHC in your account
+        </h1>
+      ) : (
+        <h1 style={{ color: "red" }}>You have {amount} GHC in your account</h1>
+      )}
+
+      <button onClick={() => setAmount(amount + 1)}>Deposit</button>
+      <button onClick={() => setAmount(amount - 1)}>Withdraw</button>
     </div>
   );
 }
